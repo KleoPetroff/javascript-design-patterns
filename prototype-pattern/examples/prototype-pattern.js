e7cadb3efbec09859648cf64d092223db84a0710
KleoPetroff <kleopetroff@gmail.com>
2015-09-17 00:21:48 +0300

    commit prototype example

---
+++ prototype-pattern/examples/prototype-pattern.js
@@ -0,0 +1,11 @@
+var Car = {
+    name: 'Ford',
+
+    drive: function() {
+        return 'I\'m driving a ' + this.name + '.';
+    },
+
+    panic: function() {
+        return 'I don\' know how to stop!!';
+    }
+};
