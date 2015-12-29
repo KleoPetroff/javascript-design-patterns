5b0beb9a7f3326bd9fa24292469ee5bca8710647
KleoPetroff <kleopetroff@gmail.com>
2015-12-29 22:29:12 +0200

    refactor all BDD tests to TDD

--- command-pattern/examples/tests/command-pattern-tests.js
+++ command-pattern/examples/tests/command-pattern-tests.js
@@ -1,28 +1,27 @@
 var expect = chai.expect;
 var assert = chai.assert;
 
-
 describe('A Command Pattern', function() {
     describe('The Command Pattern', function() {
         it('should exist', function() {
-            expect(inventoryManager).to.be.ok;
+            assert.ok(inventoryManager);
             assert.isObject(inventoryManager);
         });
         it('should have an add method', function() {
-            expect(inventoryManager.add).to.be.ok;
+            assert.ok(inventoryManager.add);
             assert.isFunction(inventoryManager.add);
         });
         it('should have delete method', function() {
-            expect(inventoryManager.remove).to.be.ok;
+            assert.ok(inventoryManager.remove);
             assert.isFunction(inventoryManager.remove);
         });
         it('should have check method', function() {
-            expect(inventoryManager.check).to.be.ok;
+            assert.ok(inventoryManager.check);
             assert.isFunction(inventoryManager.check);
         });
 
         it('should have execute method', function() {
-            expect(inventoryManager.execute).to.be.ok;
+            assert.ok(inventoryManager.execute);
             assert.isFunction(inventoryManager.execute);
         });
 
@@ -31,9 +30,9 @@ describe('A Command Pattern', function() {
             var testInventoryRemove = inventoryManager.execute('remove', 'Harry Potter', '33214');
             var testInventoryCheck = inventoryManager.execute('check', 'Harry Potter', '33214');
 
-            expect(testInventoryAdd, 'You have successfully added Harry Potter with ID: 33214');
-            expect(testInventoryRemove, 'You have successfully removed Harry Potter with ID: 33214');
-            expect(testInventoryCheck, 'You have successfully checked Harry Potter with ID: 33214');
+            assert.equal(testInventoryAdd, 'You have successfully added Harry Potter with ID: 33214');
+            assert.equal(testInventoryRemove, 'You have successfully removed Harry Potter with ID: 33214');
+            assert.equal(testInventoryCheck, 'You have selected Harry Potter with ID: 33214');
         });
     });
 });
